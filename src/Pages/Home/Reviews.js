2bca7a7493bbae2fe974e05f0e604433f78cad6c
ishtiyak98 <rifatishtiyak@gmail.com>
2022-05-24 19:53:06 +0600

    reviews section edited

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -3,6 +3,8 @@ import { useQuery } from "react-query";
 import Spinner from "../Shared/Spinner";
 import "react-slideshow-image/dist/styles.css";
 import { Slide } from "react-slideshow-image";
+import { FaStar } from 'react-icons/fa';
+
 
 const Reviews = () => {
   const { data: reviews, isLoading } = useQuery("reviews", () =>
@@ -26,20 +28,28 @@ const Reviews = () => {
             <Slide className=" mx-auto">
               {reviews.map((review, index) => (
                 <div className="each-slide" key={index}>
-                  <div className="px-32">
+                  <div className="px-10 lg:px-32">
                     <div className="avatar flex justify-center my-6">
-                      <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                      <div className="w-28 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                         <img
                           src="https://api.lorem.space/image/face?hash=3174"
                           alt=""
                         />
                       </div>
                     </div>
-                    <p className="text-center italic text-2xl text-slate-500 font-semibold mb-4">
+                    <p className="text-center italic text-lg lg:text-2xl text-slate-500 font-semibold mb-4">
                       "{review.description}"
                     </p>
-                    <h4 className="text-center italic text-primary text-xl font-medium">
-                      {review.name}
+                    <div className="mx-auto flex justify-center mb-3">
+                        <div className="flex text-primary">
+                            {
+                                [...Array(parseInt(review.ratings))].map( (item,index) => <FaStar/>)
+                                
+                            }
+                        </div>
+                    </div>
+                    <h4 className="text-center italic text-primary text-lg font-medium">
+                        Ratings: {review.ratings}/5
                     </h4>
                   </div>
                 </div>
