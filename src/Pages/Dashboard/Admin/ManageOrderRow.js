c2406ca678d51b3e3f0e9baa3ac34cadbfdd312c
ishtiyak98 <rifatishtiyak@gmail.com>
2022-05-25 23:37:44 +0600

    manage all order done

--- src/Pages/Dashboard/Admin/ManageOrderRow.js
+++ src/Pages/Dashboard/Admin/ManageOrderRow.js
@@ -1,28 +1,89 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import Swal from 'sweetalert2';
 
-const ManageOrderRow = ({index, order}) => {
-    return (
-        <tr>
-      <td>{index + 1}</td>
-      <td>{order.itemName}</td>
-      <td>{order.email}</td>
-      <td>{order.paymentStatus}</td>
-      <td>
-          {
-            order.paymentStatus==="paid" ?"shipped" :"waiting for payment"
+const ManageOrderRow = ({index, order, refetch}) => {
+
+  const handleShipped = (id) => {
+    fetch(`http://localhost:5000/orderShipped/${id}`, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        // body: JSON.stringify(updateInfo),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.acknowledged) {
+            refetch();
+            toast.success("Order shipped");
           }
-      </td>
-      <td className="space-x-3">
-        <button
-          className="btn btn-sm btn-primary text-white font-medium rounded-none disabled:bg-slate-200 disabled:text-black"
-        >
-          Shipped
-        </button>
-        <button className="btn btn-sm btn-primary text-white font-medium rounded-none">
-          Remove Order
-        </button>
-      </td>
-    </tr>
+        });
+    
+  }
+
+  //!------------- remove an unpaid order --------------
+  const handleRemoveOrder = (id) => {
+    Swal.fire({
+      title: "Cancel Order",
+      text: `Do you want to cancel ${order.itemName}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+      cancelButtonText: "No",
+      iconColor : "#FF6A00"
+    }).then((result) => {
+
+      if (result.isConfirmed) {
+          fetch(`http://localhost:5000/order/${id}`, {
+            method:"DELETE",
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            }
+          })
+          .then(res => res.json())
+          .then(data => {
+            if (data.acknowledged) {
+              toast.success("Item is cancelled")
+              refetch();  
+            }
+          })
+      }
+    })
+  }
+
+    return (
+      <tr>
+        <td>{index + 1}</td>
+        <td>{order.itemName}</td>
+        <td>{order.email}</td>
+        <td>
+          {
+            order.paymentStatus === "paid" ? <span className="text-green-500">{order.paymentStatus}</span> : <span className="text-red-500">{order.paymentStatus}</span>
+          }</td>
+        <td>
+            {
+              order.orderStatus ? <span className="text-green-500">{order.orderStatus}</span> : <span className="text-red-500">{"pending"}</span>
+            }
+        </td>
+        <td className="space-x-3">
+          <button
+            className="btn btn-sm btn-primary text-white font-medium rounded-none disabled:bg-slate-200 disabled:text-black"
+            disabled={order.paymentStatus === "unpaid"}
+            onClick={()=>{handleShipped(order._id)}}
+          >
+            Shipped
+          </button>
+          <button 
+            className="btn btn-sm btn-primary text-white font-medium rounded-none disabled:bg-slate-200 disabled:text-black"
+            disabled={order.paymentStatus === "paid"}
+            onClick={()=>{handleRemoveOrder(order._id)}}
+            >
+            Remove Order
+          </button>
+        </td>
+      </tr>
     );
 };
 
--- src/Pages/Dashboard/Admin/ManageOrders.js
+++ src/Pages/Dashboard/Admin/ManageOrders.js
@@ -24,8 +24,8 @@ const ManageOrders = () => {
       </h4>
 
 
-      <div class="overflow-x-auto">
-        <table class="table w-full">
+      <div className="overflow-x-auto">
+        <table className="table w-full">
           <thead>
             <tr>
               <th className="text-base">Sl.</th>
